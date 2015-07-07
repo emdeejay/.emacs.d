f1e7c91fb33a21997671ad2278752be9a1f995ef
emdeejay <mdj.mdj@gmail.com>
2015-07-07 20:37:52 +1000

    Need this as well genius

---
+++ mylisp/installed-packages-list.el
@@ -0,0 +1,3 @@
+(setq installed-packages '(flymake-cursor flymake-easy flymake-python-pyflakes ir-black-theme puppet-mode puppetfile-mode scala-mode wisi avy ace-window))
+
+(provide 'installed-packages-list)
